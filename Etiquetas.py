665be22381136ede30565c41262a093a8f710786
Yeytan <esdras.yeytan@gmail.com>
2023-03-09 12:50:30 -0600

    Actualizar

---
+++ Etiquetas.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+
+raiz = Tk()
+etqTexto = ttk.Label( raiz, text="Regular show Mordecai")
+etqTexto.grid()
+
+imagen = PhotoImage(file="regularshow.png")
+
+etqImagen = ttk.Label(raiz)
+etqImagen.grid()
+etqImagen ['image'] = imagen
+
+etqCombinada = ttk.Label(raiz, text="Mordecai", compound= "top") #compund acomoda la imagen de acuerdo al texto.
+etqCombinada.grid()
+etqCombinada['image'] = imagen
+
+raiz.mainloop()
